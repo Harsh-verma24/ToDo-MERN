8109b69720479e437093995c3d7ec736dfa76649
Harsh-verma24 <iamharshvrma@gmail.com>
2025-07-28 10:21:25 +0530

    env file

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',  // Change this to your API base URL
+  baseURL: import.meta.env.VITE_API_URL,  // Change this to your API base URL
   headers: {
     'Content-Type': 'application/json',
   },
