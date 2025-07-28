73baa4dd0cbdf13b484a34ebd6452bc70c378326
Harsh-verma24 <iamharshvrma@gmail.com>
2025-07-28 10:50:51 +0530

    api issue solve

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,  // Change this to your API base URL
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,  // Change this to your API base URL
   headers: {
     'Content-Type': 'application/json',
   },
